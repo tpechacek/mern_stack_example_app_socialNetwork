cedec2ba45262d6eb027f632f6257d79eb3dbb25
Taylor Pechacek <taylortpechacek@gmail.com>
2020-03-07 22:37:58 +1100

    Starting on User model and iniital routes with some validation

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+
+// Allows us to get the data in request.body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 

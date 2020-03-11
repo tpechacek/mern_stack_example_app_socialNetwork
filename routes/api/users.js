9de8596734151ed3c8c858ebf2416ed41e6d5204
Taylor Pechacek <taylortpechacek@gmail.com>
2020-03-11 22:46:20 +1100

    adding some updates to users routes and some new middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // Bring in User model
@@ -63,7 +65,22 @@ router.post(
       await user.save();
 
       // Return the json web token (JWT)
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        // Reminder to change back to 3600 for production.
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');

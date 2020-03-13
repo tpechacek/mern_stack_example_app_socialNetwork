c0e2c4f76ee88ba86a27e836ca8220f79edc5fc4
Taylor Pechacek <taylortpechacek@gmail.com>
2020-03-13 17:26:52 +1100

    Finished new PUT route for creating experience entry in your profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -167,4 +167,68 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route     PUT api/profile/experience
+// @desc      Add profile experience
+// @access    Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title, // Same as title = title
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      // Get the profile you are looking for.
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      // Take that profile and append the empty experience array using
+      // unshift() which adds to beginning of the array.
+      // Use the 'newExp' object created above from the req.body destructuring before it.
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
